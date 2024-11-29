caf6a52098fb0a227352d280ceff10fbaa449df0
KagonKhan <s.szynk98@gmail.com>
2024-11-29 11:47:02 +0100

    iniital outline

---
+++ GameScriptingEngine/main.cpp
@@ -0,0 +1,24 @@
+#include <print>
+
+
+
+
+
+/*
+	IDEAS:
+	- OCR, ImageSearch (edges?) 
+	- AI (NEAT algorithms support)
+	- MOUSE, KEYBOARD facilities
+	- SHORTCUTS to scripts
+	- MINIMIZE TO TRAY
+	- SCRIPTING engine (record mouse, kb, steps)
+	- TIMERS
+
+*/
+
+
+
+int main() {
+
+	std::print("Hello GSE");
+}
