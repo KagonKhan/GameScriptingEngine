e74a4173f025c534bcc6bf66cbf6adf11e23082f
KagonKhan <s.szynk98@gmail.com>
2024-12-02 02:58:58 +0100

    readme

--- GameScriptingEngine/main.cpp
+++ GameScriptingEngine/main.cpp
@@ -1,21 +1,6 @@
 #include "App.hpp"
 #include <spdlog/spdlog.h>
 
-
-
-
-/*
-	IDEAS:
-	- OCR, ImageSearch (edges?) 
-	- AI (NEAT algorithms support)
-	- MOUSE, KEYBOARD facilities
-	- SHORTCUTS to scripts
-	- MINIMIZE TO TRAY
-	- SCRIPTING engine (record mouse, kb, steps)
-	- TIMERS
-
-*/
-
 void initialize() { 
     spdlog::set_level(spdlog::level::debug);
 
@@ -25,8 +10,5 @@ int main() {
     initialize();
 
 	App app;
-
-
-
-	app.start();
+    app.start();
 }
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # GameScriptingEngine
+
+### Simple Autoclicker
+
+
+
+ ### IDEAS:
+- OCR, ImageSearch (edges?) 
+- AI (NEAT algorithms support)
+- MOUSE, KEYBOARD facilities (basic implemented)
+- AUTOCLICKER (basic implemented)
+- SHORTCUTS to scripts (started)
+- MINIMIZE TO TRAY
+- SCRIPTING engine (record mouse, kb, steps)
+- TIMERS
